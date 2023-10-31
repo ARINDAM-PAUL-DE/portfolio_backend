034298461a77ab8272bbbfddf345d00c0be5d6ae
ARINDAM-PAUL-DE <arindampaul526@gmail.com>
2023-10-31 11:17:34 +0530

    backend add

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,10 @@
-export const schemaTypes = []
+import abouts from "./abouts"
+import brands from "./brands"
+import contact from "./contact"
+import experiences from "./experiences"
+import skills from "./skills"
+import testimonials from "./testimonials"
+import workExperience from "./workExperience"
+import works from "./works"
+
+export const schemaTypes = [works, testimonials, brands, abouts, skills, workExperience, experiences, contact]
